e147da05f66a5f3f63fdf9151904f44f6f81dae9
Daniel <danieldaniyyelda@gmail.com>
2017-12-31 15:17:09 -0400

    fix label name

--- ContactListDemo/ContactListDemo/ContactListDemo.iOS/Service/ContactService.cs
+++ ContactListDemo/ContactListDemo/ContactListDemo.iOS/Service/ContactService.cs
@@ -11,59 +11,61 @@ namespace ContactListDemo.iOS.Service
 {
     public class ContactService : IContactService
     {
-        private static IEnumerable<Contact> _contacts;
-        public ContactService()
+        public class ContactService : IContactService
         {
-        }
-
-        public List<Contact> FindContacts(string searchInContactsString)
-        {
-            var ResultContacts = new List<Contact>();
-
-            foreach (var currentContact in _contacts)
+            private static IEnumerable<DirectoryContact> _contacts;
+            public ContactService()
             {
-                if ((currentContact.Contact_DisplayName != null && currentContact.Contact_DisplayName.ToLower().Contains(searchInContactsString.ToLower())) ||
-                    (currentContact.Contact_Number != null && currentContact.Contact_Number.ToLower().Contains(searchInContactsString.ToLower())))
-                {
-                    ResultContacts.Add(currentContact);
-                }
             }
 
-            return ResultContacts;
-        }
-
-        public async Task<IEnumerable<Contact>> GetAllContacts()
-        {
-            if (_contacts != null) return _contacts;
-
-            var contacts = new List<Contact>();
-
-            var keysTOFetch = new[] { CNContactKey.GivenName, CNContactKey.PhoneNumbers };
-            NSError error;
-            CNContact[] contactList;
-            var ContainerId = new CNContactStore().DefaultContainerIdentifier;
-            using (var predicate = CNContact.GetPredicateForContactsInContainer(ContainerId))
-
-            using (var store = new CNContactStore())
+            public async Task<List<DirectoryContact>> FindContacts(string searchInContactsString)
             {
-                contactList = store.GetUnifiedContacts(predicate, keysTOFetch, out error);
-            }
+                var ResultContacts = new List<DirectoryContact>();
 
-            foreach (var item in contactList)
-            {
-                if (null != item && null != item.PhoneNumbers)
+                foreach (var currentContact in _contacts)
                 {
-                    contacts.Add(new Contact
+                    if ((currentContact.Contact_DisplayName != null && currentContact.Contact_DisplayName.ToLower().Contains(searchInContactsString.ToLower())) ||
+                        (currentContact.Contact_Number != null && currentContact.Contact_Number.ToLower().Contains(searchInContactsString.ToLower())))
                     {
-                        Contact_DisplayName = item.GivenName,
-                        Contact_Number = item.PhoneNumbers.FirstOrDefault().ToString()
-                    });//TODO prove it in iOS, please
+                        ResultContacts.Add(currentContact);
+                    }
                 }
+
+                return ResultContacts;
             }
-            
-            _contacts = (from c in contacts orderby c.Contact_DisplayName select c).ToList();
-            
-            return _contacts;
+
+            public async Task<IEnumerable<DirectoryContact>> GetAllContacts()
+            {
+                if (_contacts != null) return _contacts;
+
+                var contacts = new List<DirectoryContact>();
+
+                var keysTOFetch = new[] { CNContactKey.GivenName, CNContactKey.PhoneNumbers };
+                NSError error;
+                CNContact[] contactList;
+                var ContainerId = new CNContactStore().DefaultContainerIdentifier;
+                using (var predicate = CNContact.GetPredicateForContactsInContainer(ContainerId))
+
+                using (var store = new CNContactStore())
+                {
+                    contactList = store.GetUnifiedContacts(predicate, keysTOFetch, out error);
+                }
+
+                foreach (var item in contactList)
+                {
+                    if (null != item && null != item.PhoneNumbers)
+                    {
+                        contacts.Add(new DirectoryContact
+                        {
+                            Contact_DisplayName = item.GivenName,
+                            Contact_Number = item.PhoneNumbers.FirstOrDefault().Value.StringValue
+                        });
+                    }
+                }
+
+                _contacts = (from c in contacts orderby c.Contact_DisplayName select c).ToList();
+
+                return _contacts;
+            }
         }
     }
-}
